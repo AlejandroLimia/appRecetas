f07cf1ea9b26559db43a4787715820a5d1325de2
Yimy LLanos <llanosyimy@gmail.com>
2020-09-14 17:53:42 -0300

    Controller de user y comments terminados , testeados

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,6 @@ const message = 'Mail or Password incorrect'
 const userController = {
 	createUser: async (req, res) => {
 		const {firstName, lastName, mail, pass, urlPic, userName } = req.body;
-		
 		// Hashing Password
 		const hashPassword = bcrypt.hashSync(pass.trim(), 10);
 
@@ -18,41 +17,42 @@ const userController = {
 			urlPic,
 			userName,
 			pass: hashPassword
-        })
+        });
 
         newUser.save()
         .then(user => {
-            jwt.sign({...user},process.env.SECRETORIKEY,{},(error, token) => {
-                if(error) return res.json({success:false, error:'An error occurred while saving the user'});
-                res.json({
-                    success: true, token, 
-                    userName: user.userName, 
-                    urlPic: user.urlPic, 
-                    likes: user.likes
-                })
-            })
-        })
-        .catch(error => res.json({success : false, error}));
-    },
-	loginUser: async (req, res) =>{
-		const {mail, pass} = req.body;
-	
-        const userExists = await User.findOne({mail});
-        if(!userExists) return (res.json({success :false, error: message}));
-
-        const passwordMatches = bcrypt.compareSync(pass, userExists.pass);
-		if(!passwordMatches) return (res.json({success :false, error: message})); 
-
-        jwt.sign({...userExists},process.env.SECRETORIKEY,{},(error, token) => {
-            if(error) return res.json({success:false, error});
+            console.log('sasasasas')
+            const token = jwt.sign({...user},process.env.SECRET_KEY,{})
+            if(!token) return res.json({success:false, error:'An error occurred while saving the user'});
             res.json({
                 success: true, token, 
                 userName: user.userName, 
                 urlPic: user.urlPic, 
                 likes: user.likes
             });
-        });	
-	},
+        })
+        .catch(err => res.json({success:'false',error:err}))
+    },
+	loginUser: async (req, res) =>{
+		const {mail, pass} = req.body;
+	
+        const userExists = await User.findOne({mail});
+        if(!userExists) return (res.json({success :false, error: message}));
+        const passwordMatches = bcrypt.compareSync(pass, userExists.pass);
+        console.log(passwordMatches)
+		if(!passwordMatches) return (res.json({success :false, error: message})); 
+
+        const token = jwt.sign({...userExists},process.env.SECRET_KEY,{})
+        
+        if(!token) return res.json({success:false, error});
+        
+        res.json({
+                success: true, token, 
+                userName: userExists.userName, 
+                urlPic: userExists.urlPic, 
+                likes: userExists.likes
+        });
+    },
 	decodeUser: (req, res) => {
 		const {urlPic, username, likes} = req.user;
 		res.json({
