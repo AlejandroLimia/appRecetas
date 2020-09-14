bf5c8590dd1b5fba28b5065eb699af4d4005bcc2
lari <larisamzalazar@gmail.com>
2020-09-14 13:45:59 -0300

    first commit

---
+++ backend/controllers/recipesController.js
@@ -0,0 +1,49 @@
+const Recipes = require('../models/Recipe');
+const Recipe = require('../models/Recipe');
+
+const recipesController = {
+    newRecipe: async (req, res) => {
+        const { title, description, ingredients, urlPic, userPic, userId, importantContains, recipe, difficulty, diet} = req.body
+        const createRecipe = new Recipe({
+            title,
+            description, 
+            ingredients, 
+            urlPic, 
+            userPic, 
+            userId, 
+            importantContains, 
+            recipe, 
+            difficulty, 
+            diet,
+        });
+        createRecipe
+        .save()
+        .then((recipe) => {
+          res.json({ success: true, recipe: recipe });
+        })
+        .catch((err) => {
+          res.json({ success: false, error: err});
+        });
+    },
+    getAllRecipes: async (req, res) => {
+        const recipes = await Recipes.find();
+        res.json({
+            success: true,
+            recipes: recipes,
+        });
+    },
+    deleteRecipe: async (req, res) =>{
+        const id = req.params.id
+        Recipes.findByIdAndDelete({_id: id})
+        .then(() => res.json({success: true, res: "Su receta ha sido eliminada."}))
+        .catch(err=>res.json({success:false, error: err}))
+    },
+    modifyRecipe: async(req, res)=>{
+        Recipes.findOneAndUpdate({_id: req.body.id},{$set:{Recipe:{ ...req.body}}})
+        .then(()=> res.json({success: true, response: "Los datos se han modificado con éxito."}))
+        .catch(err => res.json({success:false, error: err}))
+    }
+
+
+
+}
