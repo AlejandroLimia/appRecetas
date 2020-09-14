bcd720ee09f602dede1ecaf2f5e0918f5b3f8843
luciana <lumendez@MacBook.local>
2020-09-14 18:46:53 -0300

    header y footer casi terminados

--- frontend/src/components/MenuHamburguesa.js
+++ frontend/src/components/MenuHamburguesa.js
@@ -1,23 +1,14 @@
-import React, { useState } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React from 'react';
 import {NavLink} from "react-router-dom"
 
 const Menuhamburguesa = (props) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen(prevState => !prevState);
 
-  const fotoBoton = require("../images/menuHamburguesa.png")
 
   return (
-    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle caret style={{backgroundColor: `white`, border: `none`}}><img src={fotoBoton} alt="menu" style={{width: `26%`}}></img></DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem><NavLink to="/Home">Home</NavLink></DropdownItem>
-        <DropdownItem divider />
-        <DropdownItem> {/*<NavLink to="/Ciudades">Ciudades</NavLink>*/}</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
+      <div id="menuCostado">
+          <NavLink to="/Home">Home</NavLink>
+      </div>
+  )
 }
 
 export default Menuhamburguesa;
