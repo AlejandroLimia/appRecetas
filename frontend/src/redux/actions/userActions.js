4c956a594006b388aa3b59fd454ed6f7a66425b8
Yimy LLanos <llanosyimy@gmail.com>
2020-09-16 18:20:12 -0300

    error de SignUp

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -3,15 +3,15 @@ import { RUTA_API } from '../../constants'
 import { toast } from 'react-toastify'
 
 const authActions = {
-	createUser: user => {
+	createUser: (user, set) => {
 		return async (dispatch, getState) => {
 			const response = await axios.post(RUTA_API+'/api/user/register', user)
-			console.log(response)
-			if(!response.data.success) {
-				if(response.data.error.indexOf('Mail') !== -1) toast.error('Mail en uso')
-				if(response.data.error.indexOf('Username') !== -1) toast.error('Usuario en uso')
-				if(response.data.error.indexOf('error') !== -1) toast.error('Opss! Algo falló, prueba de nuevo')
-				return response.data.error
+            if(response.data.success === "false") {
+                set({status: false})
+                let errors = response.data.error.errors;
+				if(errors.username !== undefined) toast.error(errors.username.message);
+				if(errors.mail !== undefined ) toast.error(errors.mail.message);
+				return;
 			}
 			else {
 				toast.success(`Cuenta creada!`)
